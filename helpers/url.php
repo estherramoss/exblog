21e67dce139931c280d2f9aacff78beffc4ab7cc
estherramoss <estherramosbocchini@gmail.com>
2023-10-05 15:44:28 -0300

    correção do erro
    .

--- helpers/url.php
+++ helpers/url.php
@@ -1,7 +1,7 @@
 <?php
 
-// Constroi a URL base do site combinando o protocolo,
+// Constrói a URL base do site combinando o protocolo,
 // nome do servidor, diretório atual e barra final.
 
-$BASE_URL = "http://".$_SERVER['SERVER NAME'] 
-.dirname($_SERVER["REQUEST_URI"]. '?').'';
+$BASE_URL = "http://" .$_SERVER['SERVER_NAME']
+.dirname($_SERVER["REQUEST_URI"].'?').'';
